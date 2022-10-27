bf5e9dae2b4bf8c7c3ddac04f198461ea91621fa
kju2405 <kju2405@khu.ac.kr>
2022-10-27 15:43:04 +0900

    Button

---
+++ Button/app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton=(Button)findViewById(R.id.button);
+
+    }
+}
